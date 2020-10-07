b4a596bc670c1ff58e89f770ab4f72de4248509b
Sakkarin Harnchanaphol <sakkarin.ha.61@ubu.ac.th>
2020-10-07 17:33:57 +0700

    Updates

---
+++ precache-manifest.bf84b7e68fafe988a3155061c5e0a924.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c550c6f66abe4758d3b142037f448cd4",
+    "url": "/projecteror-math/index.html"
+  },
+  {
+    "revision": "f49fc0ad653d6f23a49f",
+    "url": "/projecteror-math/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "df804eab6bda7c3e89a5",
+    "url": "/projecteror-math/static/js/2.0938f15a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/projecteror-math/static/js/2.0938f15a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f49fc0ad653d6f23a49f",
+    "url": "/projecteror-math/static/js/main.613c9931.chunk.js"
+  },
+  {
+    "revision": "7c914f78606e308748d2",
+    "url": "/projecteror-math/static/js/runtime-main.11238411.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/projecteror-math/static/media/logo.ee7cd8ed.svg"
+  }
+]);
